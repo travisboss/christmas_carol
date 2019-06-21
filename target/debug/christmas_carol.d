3cdfc96457dda6d1cd88ec3e9255bec37364e8ef
travisboss <travisboss@gmail.com>
2019-06-21 11:22:29 -0500

    cargo run

--- target/debug/christmas_carol.d
+++ target/debug/christmas_carol.d
@@ -1,1 +1,1 @@
-/Users/tboss/Code/rust/loops/christmas_carol/target/debug/christmas_carol.dSYM: /Users/tboss/Code/rust/loops/christmas_carol/src/main.rs
+/Users/tboss/Code/rust/christmas_carol/target/debug/christmas_carol.dSYM: /Users/tboss/Code/rust/christmas_carol/src/main.rs
