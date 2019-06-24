b12820bc5c0a6ebdbcdac9715fd1fe8d41250504
Travis <travisboss@gmail.com>
2019-06-24 00:01:22 -0500

    spelling error

---
+++ target/debug/deps/christmas_carol-8eb3970504cda147.d
@@ -0,0 +1,5 @@
+/Users/Boss/code/rust/christmas_carol/target/debug/deps/christmas_carol-8eb3970504cda147.rmeta: src/main.rs
+
+/Users/Boss/code/rust/christmas_carol/target/debug/deps/christmas_carol-8eb3970504cda147.d: src/main.rs
+
+src/main.rs:
